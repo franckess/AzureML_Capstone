c66260586f300cb352c9bcdc64e310777f0438a6
Rene Essomba <franckess48@gmail.com>
2021-04-11 13:26:53 +1000

    new commit: 11/04

---
+++ score.py
@@ -0,0 +1,56 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+import json
+import logging
+import os
+import pickle
+import numpy as np
+import pandas as pd
+import joblib
+
+import azureml.automl.core
+from azureml.automl.core.shared import logging_utilities, log_server
+from azureml.telemetry import INSTRUMENTATION_KEY
+
+from inference_schema.schema_decorators import input_schema, output_schema
+from inference_schema.parameter_types.numpy_parameter_type import NumpyParameterType
+from inference_schema.parameter_types.pandas_parameter_type import PandasParameterType
+
+
+input_sample = pd.DataFrame({"n_tokens_title": pd.Series([0.0], dtype="float64"), "n_tokens_content": pd.Series([0.0], dtype="float64"), "n_unique_tokens": pd.Series([0.0], dtype="float64"), "num_hrefs": pd.Series([0.0], dtype="float64"), "num_self_hrefs": pd.Series([0.0], dtype="float64"), "num_imgs": pd.Series([0.0], dtype="float64"), "num_videos": pd.Series([0.0], dtype="float64"), "average_token_length": pd.Series([0.0], dtype="float64"), "num_keywords": pd.Series([0.0], dtype="float64"), "data_channel_is_entertainment": pd.Series([0], dtype="int64"), "data_channel_is_bus": pd.Series([0], dtype="int64"), "data_channel_is_socmed": pd.Series([0], dtype="int64"), "data_channel_is_tech": pd.Series([0], dtype="int64"), "data_channel_is_world": pd.Series([0], dtype="int64"), "kw_min_min": pd.Series([0.0], dtype="float64"), "kw_max_min": pd.Series([0.0], dtype="float64"), "kw_min_max": pd.Series([0.0], dtype="float64"), "kw_avg_max": pd.Series([0.0], dtype="float64"), "kw_min_avg": pd.Series([0.0], dtype="float64"), "kw_max_avg": pd.Series([0.0], dtype="float64"), "kw_avg_avg": pd.Series([0.0], dtype="float64"), "self_reference_min_shares": pd.Series([0.0], dtype="float64"), "self_reference_max_shares": pd.Series([0.0], dtype="float64"), "weekday_is_wednesday": pd.Series([0], dtype="int64"), "weekday_is_saturday": pd.Series([0], dtype="int64"), "weekday_is_sunday": pd.Series([0], dtype="int64"), "is_weekend": pd.Series([0], dtype="int64"), "LDA_00": pd.Series([0.0], dtype="float64"), "LDA_01": pd.Series([0.0], dtype="float64"), "LDA_02": pd.Series([0.0], dtype="float64"), "LDA_03": pd.Series([0.0], dtype="float64"), "LDA_04": pd.Series([0.0], dtype="float64"), "global_subjectivity": pd.Series([0.0], dtype="float64"), "global_sentiment_polarity": pd.Series([0.0], dtype="float64"), "global_rate_positive_words": pd.Series([0.0], dtype="float64"), "global_rate_negative_words": pd.Series([0.0], dtype="float64"), "rate_positive_words": pd.Series([0.0], dtype="float64"), "rate_negative_words": pd.Series([0.0], dtype="float64"), "avg_positive_polarity": pd.Series([0.0], dtype="float64"), "min_positive_polarity": pd.Series([0.0], dtype="float64"), "max_positive_polarity": pd.Series([0.0], dtype="float64"), "avg_negative_polarity": pd.Series([0.0], dtype="float64"), "min_negative_polarity": pd.Series([0.0], dtype="float64"), "max_negative_polarity": pd.Series([0.0], dtype="float64"), "title_subjectivity": pd.Series([0.0], dtype="float64"), "title_sentiment_polarity": pd.Series([0.0], dtype="float64"), "abs_title_sentiment_polarity": pd.Series([0.0], dtype="float64")})
+output_sample = np.array([0])
+try:
+    log_server.enable_telemetry(INSTRUMENTATION_KEY)
+    log_server.set_verbosity('INFO')
+    logger = logging.getLogger('azureml.automl.core.scoring_script')
+except:
+    pass
+
+
+def init():
+    global model
+    # This name is model.id of model that we want to deploy deserialize the model file back
+    # into a sklearn model
+    model_path = os.path.join(os.getenv('AZUREML_MODEL_DIR'), 'model.pkl')
+    path = os.path.normpath(model_path)
+    path_split = path.split(os.sep)
+    log_server.update_custom_dimensions({'model_name': path_split[-3], 'model_version': path_split[-2]})
+    try:
+        logger.info("Loading model from path.")
+        model = joblib.load(model_path)
+        logger.info("Loading successful.")
+    except Exception as e:
+        logging_utilities.log_traceback(e, logger)
+        raise
+
+
+@input_schema('data', PandasParameterType(input_sample))
+@output_schema(NumpyParameterType(output_sample))
+def run(data):
+    try:
+        result = model.predict(data)
+        return json.dumps({"result": result.tolist()})
+    except Exception as e:
+        result = str(e)
+        return json.dumps({"error": result})
--- train.py
+++ train.py
@@ -2,6 +2,7 @@ from __future__ import print_function
 import os
 import warnings
 import argparse
+import joblib
 import pingouin as pg
 import numpy as np
 import requests
@@ -12,14 +13,14 @@ from io import BytesIO
 from boruta import BorutaPy
 from azureml.core.run import Run
 from urllib.request import urlopen
-from sklearn.externals import joblib
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 warnings.filterwarnings(action='ignore', category=UserWarning, module='lightgbm')
 
-def corr_drop_cols(df, corr_val = 0.75):
+def corr_drop_cols(df, corr_val = 0.85):
     df_copy = df.copy() # create a copy
 
     corrmat = pg.pairwise_corr(df_copy, method='pearson')[['X', 'Y', 'r']]
@@ -34,11 +35,17 @@ def corr_drop_cols(df, corr_val = 0.75):
 def create_label(df):
     df_copy = df.copy() # create a copy
 
-    # Encoding categorical data
+    # Encoding target variable
     df_copy['label'] = [1 if x >= 1400 else 0 for x in df_copy['shares']]
+    df_copy = df_copy.drop(['shares', 'timedelta'], axis=1)
+
     y = df_copy['label'].values
     labelencoder = LabelEncoder()
     df_copy['label'] = labelencoder.fit_transform(y)
+
+    # Encoding all categorical variables
+    col_list = [s for s in df_copy.columns if 'is' in s]
+    df_copy[col_list] = df_copy[col_list].apply(lambda x: labelencoder.fit_transform(x))
 
     return df_copy
 
@@ -47,7 +54,7 @@ def scaling_num(df):
 
     # Scaling numerical data
     from sklearn.preprocessing import MinMaxScaler
-    col_list = [s for s in df_copy.columns if 'is' in s] + ['timedelta', 'shares', 'label']
+    col_list = [s for s in df_copy.columns if 'is' in s] + ['label']
     num_cols = [m for m in df_copy if m not in col_list]
     scale = MinMaxScaler()
     df_copy[num_cols] = pd.DataFrame(scale.fit_transform(df_copy[num_cols].values), columns=[num_cols], index=df_copy.index)
@@ -59,42 +66,37 @@ def feature_selection(df, OUT_LOC):
     
     mfile = BytesIO(requests.get(OUT_LOC).content) # BytesIO create a file object out of the response from GitHub 
     feat_selector = joblib.load(mfile)
-    X = df_copy.drop(['shares', 'timedelta', 'label'], axis=1)
-    keep_cols = list(X.columns[feat_selector.support_]) + ['timedelta','label']
+    X = df_copy.drop(['label'], axis=1)
+    keep_cols = list(X.columns[feat_selector.support_]) + ['label']
 
     df_copy = df_copy[keep_cols]
     
     return df_copy
 
-def split_train_test(df, qt = 0.8):
+def split_train_test(df):
     df_copy = df.copy() # create a copy
 
-    split_timedelta = df_copy['timedelta'].quantile(qt)
-
-    train_df = df_copy[df_copy['timedelta'] <= split_timedelta]
-    test_df = df_copy[df_copy['timedelta'] > split_timedelta]
-
-    X_train = train_df.drop(['timedelta', 'label'], axis=1)
-    X_test = test_df.drop(['timedelta', 'label'], axis=1)
-    y_train = train_df['label']
-    y_test = test_df['label']
+    X = df_copy.drop('label', axis=1)
+    y = df_copy.pop('label')
+    # Train-test split 80/20
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, stratify = y, random_state = 100)
 
     return X_train, X_test, y_train, y_test
 
 if __name__ == "__main__":
     
     print('azureml.core.VERSION={}'.format(azureml.core.VERSION))
-    # Parse input arguments
+    # Parse ainput arguments
     parser = argparse.ArgumentParser()
-    parser.add_argument("--data-folder", type=str, dest='data_folder', default=".", help="data folder")
-    parser.add_argument("--boruta-model", type=str, dest='boruta_model', default="https://github.com/franckess/AzureML_Capstone/releases/download/1.0/boruta_model.pkl", help="boruta folder")
+    #parser.add_argument("--data-folder", type=str, dest='data_folder', default="https://github.com/franckess/AzureML_Capstone/blob/main/data/OnlineNewsPopularity.csv?raw=true", help="data folder")
+    #parser.add_argument("--boruta-model", type=str, dest='boruta_model', default="https://github.com/franckess/AzureML_Capstone/releases/download/1.1/boruta_model_final.pkl", help="boruta folder")
     parser.add_argument("--num-leaves", type=int, dest="num_leaves", default=64, help="# of leaves of the tree")
     parser.add_argument("--min-data-in-leaf", type=int, dest="min_data_in_leaf", default=50, help="minimum # of samples in each leaf")
     parser.add_argument("--learning-rate", type=float, dest="learning_rate", default=0.001, help="learning rate")
     parser.add_argument("--feature-fraction",type=float,dest="feature_fraction",default=1.0,help="ratio of features used in each iteration")
     parser.add_argument("--bagging-fraction",type=float,dest="bagging_fraction",default=1.0,help="ratio of samples used in each iteration")
     parser.add_argument("--bagging-freq", type=int, dest="bagging_freq", default=1, help="bagging frequency")
-    parser.add_argument("--max-rounds", type=int, dest="max_rounds", default=400, help="# of boosting iterations")
+    parser.add_argument("--max-depth", type=int, dest="max_depth", default=20, help="To limit the tree depth explicitly")
     args = parser.parse_args()
     args.feature_fraction = round(args.feature_fraction, 2)
     args.bagging_fraction = round(args.bagging_fraction, 2)
@@ -104,10 +106,10 @@ if __name__ == "__main__":
     run = Run.get_context()
 
     # Data location
-    DATA_LOC = args.data_folder
+    DATA_LOC = "https://github.com/franckess/AzureML_Capstone/blob/main/data/OnlineNewsPopularity.csv?raw=true"
 
     # Boruta model location
-    BORUTA_LOC = args.boruta_model
+    BORUTA_LOC = "https://github.com/franckess/AzureML_Capstone/releases/download/1.1/boruta_model_final.pkl"
 
     
   # Parameters of GBM model
@@ -119,9 +121,8 @@ if __name__ == "__main__":
         "feature_fraction": args.feature_fraction,
         "bagging_fraction": args.bagging_fraction,
         "bagging_freq": args.bagging_freq,
-        "num_rounds": args.max_rounds,
-        "early_stopping_rounds": 125,
         "num_threads": 16,
+        "max_depth": args.max_depth
     }
     
     print(params)
@@ -137,22 +138,23 @@ if __name__ == "__main__":
     df = df.drop(['url'], axis=1)
         
     # Perform Data pre-processing
+    print('Running pre-processing steps')
     df = corr_drop_cols(df)
 
     df = create_label(df)
 
     df = scaling_num(df)
 
+    print('Loading Boruta model for feature selection from {}'.format(BORUTA_LOC))
     df = feature_selection(df, BORUTA_LOC)
     
     # Split train data into train & test
     X_train, X_test, y_train, y_test = split_train_test(df)
-    
-    d_train = lgb.Dataset(X_train, y_train)
-    d_val = lgb.Dataset(X_test, y_test)
 
     # Train LightGBM model
-    clf = lgb.LGBMClassifier(params, d_train, 100, categorical_feature="auto")
+    print('Running modeling step')
+    clf = lgb.LGBMClassifier(**params)
+    clf.fit(X_train, y_train)
 
     #prediction on the test set
     y_pred=clf.predict(X_test)
@@ -162,10 +164,9 @@ if __name__ == "__main__":
     print('LightGBM Model accuracy score: {0:0.4f}'.format(accuracy_score(y_test, y_pred)))
     
     # Log the validation loss (NRMSE - normalized root mean squared error)
-    run.log("Model accuracy", np.float(accuracy_score(y_test, y_pred)))
+    run.log("Accuracy", np.float(accuracy))
  
  
     #Dump the model using joblib
-    os.makedirs("./output/", exist_ok=True)
-    model_path = os.path.join("./output/", 'lightgbm_model.pkl')
-    joblib.dump(clf, filename=model_path)
+    os.makedirs("outputs", exist_ok=True)
+    joblib.dump(value=clf, filename="outputs/lgb_model.pkl")
